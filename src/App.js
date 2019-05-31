ab2d19d72234d190b8cc126f59977045e0800d29
Prasad178 <50615066+Prasad178@users.noreply.github.com>
2019-05-30 21:37:44 -0500

    Updated

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import ImageFile from './imageFile';
 import State from './States'
 import UserInput from './UserInput';
 import UserOutput from './UserOutput';
+import './UserOutput.css';
 
  export default class App extends Component{
 
   state={
-    username:"Sanjiv",
-    password:"Science and Soccer"
+    username:"Latha",
+    password:"Prasad"
   }
 
 changeEventHandler=()=>
@@ -17,21 +18,58 @@ changeEventHandler=()=>
  this.setState({username: "Prasad",password: "Sports and investigation movies"})
 
 }
+
+clickedLoginHandlerSanjiv=()=>
+{
+  this.setState({username: "Latha",password: "LathaSanjiv"})
+
+}
+clickedLoginHandlerShivaani=()=>
+{
+  this.setState({username: "Latha",password: "LathaShivaani"})
+
+}
+clickedLoginHandlerPrasad=()=>
+{
+  this.setState({username: "Latha",password: "LathaPrasad"})
+
+}
+
 render()
 {
   return (
     <div className="App">
 
       <header className="App-header">
-
                 Welcome to our family website
-                <UserInput changed={this.changeEventHandler} name={this.state.username}/>
-                  <ImageFile name="HappyBirthday.png"/>
-            
-
+                <UserInput changed={this.changeEventHandler} name={this.state.username}/>&nbsp;&nbsp;
+                <p className="UserOutput"><button onClick={this.clickedLoginHandlerSanjiv}>Sanjiv </button>&nbsp;&nbsp;
+               <button onClick={this.clickedLoginHandlerShivaani}>Shivaani </button>&nbsp;&nbsp;
+              <button onClick={this.clickedLoginHandlerPrasad}>Prasad </button></p>&nbsp;&nbsp;
+                {
+                  this.state.password=="LathaShivaani"?
+                <div>
+                <ImageFile name="LathaShivaani.png"/>
                 <ImageFile name="sample.gif"/>
-
                 <State/>
+                </div>:null
+                }
+                {
+                  this.state.password=="LathaSanjiv"?
+                <div>
+                <ImageFile name="HappyBirthday.png"/>
+                <ImageFile name="sample.gif"/>
+                <State/>
+                </div>:null
+                }
+                {
+                  this.state.password=="LathaPrasad"?
+                <div>
+                <ImageFile name="LathaPrasad.jpg"/>
+                <ImageFile name="sample.gif"/>
+                <State/>
+                </div>:null
+                }
 
                 </header>
 
--- src/UserInput.js
+++ src/UserInput.js
@@ -10,7 +10,8 @@ return(
 
   <div>
   <p>  UserName: <input type="text" name="UserName" onChange={props.changed} value={props.name} style={style}/>&nbsp;
-      Password: <input type="text" name="Password" style={style}/>
+      Password: <input type="text" name="Password" style={style}/>&nbsp;&nbsp;&nbsp;
+      
       </p>
   </div>
 )
